57ec4683a45df89ad46485c8f036850fa601aa21
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-21 23:22:11 +0000

    fetching people,vehciles,and planets from SWAPI successful

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,27 @@ export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
 
+const getPeople =  ()=>{
+fetch("https://www.swapi.tech/api/people")
+.then((resp)=>resp.json())
+.then((data)=>console.log(data.results, "MY DATA!"));
+console.log(useGlobalReducer());
+}
+
+const getVehicles= ()=>{
+fetch("https://www.swapi.tech/api/vehicles")
+.then((resp)=>resp.json())
+.then((data)=>console.log(data.results, "MY DATA!"));
+console.log(useGlobalReducer());
+}
+
+const getPlanets=()=>{
+fetch("https://www.swapi.tech/api/planets")
+.then((resp)=>resp.json())
+.then((data)=>console.log(data.results, "MY DATA!"));
+console.log(useGlobalReducer());
+}
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
